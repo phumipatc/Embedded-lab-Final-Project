e88661d6d1916226ad0582c2119e71e3abe8f61b
Tawatwee Aiemarreerat <77015953+PunnyOz2@users.noreply.github.com>
2023-05-18 11:37:22 +0700

    Update sketch_apr17a.ino

--- nodeMCU/sketch_apr17a.ino
+++ nodeMCU/sketch_apr17a.ino
@@ -1,63 +1,21 @@
 #include <SoftwareSerial.h>
 #include <ESP8266WiFi.h>
-#include <Firebase_ESP_Client.h>
-#include <ArduinoJson.h>
+#include <FirebaseESP8266.h>
 
 EspSoftwareSerial::UART testSerial;
 
 // WiFi credentials
-const char* ssid = "everyday_space_user";
-const char* password = "XGWN7rrg";
+const char* ssid = "Pun mai ngub";
+const char* password = "punpunpun3";
 
-// Firebase Credentials
-#define FIREBASE_HOST "your-project-id.firebaseio.com"
-#define FIREBASE_AUTH "YourFirebaseAuthToken"
-#define COLLECTION_NAME = "dataFromNodeMCU"
+// Set your Firebase project's unique identifier
+const char* firebaseHost = "embbeded-final-project-water-default-rtdb.asia-southeast1.firebasedatabase.app";
 
-// Define the Firebase Data object
-FirebaseData fbdo;
+// Set your Firebase project's authentication credentials
+const char* firebaseAuth = "WtZpws8GzsHQx41SxqoM3wqTODSeMAiDHoGbCzDa";
 
-// Define the FirebaseAuth data for authentication data
-FirebaseAuth auth;
-
-// Define the FirebaseConfig data for config data
-FirebaseConfig config;
-
-// Assign the project host and api key 
-config.host = FIREBASE_HOST;
-config.api_key = API_KEY;
-
-config.signer.test_mode = true;
-
-// Assign the user sign in credentials
-auth.user.email = USER_EMAIL;
-auth.user.password = USER_PASSWORD;
-
-// Optional, set AP reconnection in setup()
-// Firebase.reconnectWiFi(true);
-
-// Optional, set number of error retry
-// Firebase.RTDB.setMaxRetry(&fbdo, 3);
-
-// Optional, set number of error resumable queues
-// Firebase.RTDB.setMaxErrorQueue(&fbdo, 30);
-
-// Optional, use classic HTTP GET and POST requests.
-// This option allows get and delete functions (PUT and DELETE HTTP requests) works for
-// device connected behind the Firewall that allows only GET and POST requests.
-// Firebase.RTDB.enableClassicRequest(&fbdo, true);
-
-// #if defined(ESP8266)
-// Optional, set the size of BearSSL WiFi to receive and transmit buffers
-// Firebase may not support the data transfer fragmentation, you may need to reserve the buffer to match
-// the data to transport.
-// fbdo.setBSSLBufferSize(1024, 1024); // minimum size is 512 bytes, maximum size is 16384 bytes
-// #endif
-
-
-// Optional, set the size of HTTP response buffer
-// Prevent out of memory for large payload but data may be truncated and can't determine its type.
-// fbdo.setResponseSize(1024); // minimum size is 1024 bytes
+// Create an instance of the Firebase Data object
+FirebaseData firebaseData;
 
 void setup() {
   Serial.begin(9600);
@@ -75,14 +33,14 @@ void setup() {
   Serial.print("IP address: ");
   Serial.println(WiFi.localIP());
 
-  testSerial.begin(115200, EspSoftwareSerial::SWSERIAL_8N1, D7, D8, false, 95, 11);
+  // testSerial.begin(115200, EspSoftwareSerial::SWSERIAL_8N1, D7, D8, false, 95, 11);
   
-  // Initialize the library with the Firebase authen and config.
-  Firebase.begin(&config, &auth);
+  // Initialize Firebase
+  Firebase.begin(firebaseHost, firebaseAuth);
 }
 char buffer[10];
 void loop() {
-  // Your code here
+
   if(testSerial.available() > 0){
     int cnt = 0;
     while(cnt != 12){
@@ -96,30 +54,41 @@ void loop() {
     buffer[cnt] = NULL;
     Serial.println(buffer);
 
-    //Send data to firebase
-    // Prepare data to send
-    int value = 0;
-    for(int i=1;i<3;i++){
-      value*=10;
-      value+=buffer[i]-'0';
+    int TDS_Value = 0;
+    for(int i=1;i<=3;i++){
+      TDS_Value*=10;
+      TDS_Value+=buffer[i]-'0';
     }
-    jsonBuffer.clear();
-    jsonBuffer.add("TDS_Value", value);
-    String json;
-    jsonBuffer.toString(json);
-
-    // Send data to Firebase
-    Firebase.setString(firebaseData, "/" + collectionName, json);
-
-    if (firebaseData.dataAvailable()) {
-      if (firebaseData.dataType() == "string") {
-        Serial.print("Data sent to Firebase: ");
-        Serial.println(firebaseData.stringData());
-      }
+    int Thermo_Value = 0;
+    for(int i=5;i<=7;i++){
+      Thermo_Value*=10;
+      Thermo_Value+=buffer[i]-'0';
     }
-
+    int DissolvedO2_Value = 0;
+    for(int i=9;i<=11;i++){
+      DissolvedO2_Value*=10;
+      DissolvedO2_Value+=buffer[i]-'0';
+    }
+    // Send the data to Firestore
+    if (Firebase.setInt(firebaseData, "/TDS_Value", TDS_Value)) {
+      Serial.println("TDS_Value sent to Firestore successfully");
+    } else {
+      Serial.println("Failed to send data to Firestore");
+      Serial.println(firebaseData.errorReason());
+    }
+    if (Firebase.setInt(firebaseData, "/Thermo_Value", Thermo_Value)) {
+      Serial.println("Thermo_Value sent to Firestore successfully");
+    } else {
+      Serial.println("Failed to send data to Firestore");
+      Serial.println(firebaseData.errorReason());
+    }
+    if (Firebase.setInt(firebaseData, "/DissolvedO2_Value", DissolvedO2_Value)) {
+      Serial.println("DissolvedO2_Value sent to Firestore successfully");
+    } else {
+      Serial.println("Failed to send data to Firestore");
+      Serial.println(firebaseData.errorReason());
+    }
     // Delay before sending the next data
-    delay(2000);
+    // delay(5000);
   }
-
 }
