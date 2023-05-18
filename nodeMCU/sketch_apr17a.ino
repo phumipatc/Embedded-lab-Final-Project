ac9ebc7ef16b5e635f65f2cb0d2aba38112bc5c8
Tawatwee Aiemarreerat <77015953+PunnyOz2@users.noreply.github.com>
2023-05-18 12:13:31 +0700

    Update sketch_apr17a.ino (I dont know the changes)

--- nodeMCU/sketch_apr17a.ino
+++ nodeMCU/sketch_apr17a.ino
@@ -33,7 +33,7 @@ void setup() {
   Serial.print("IP address: ");
   Serial.println(WiFi.localIP());
 
-  // testSerial.begin(115200, EspSoftwareSerial::SWSERIAL_8N1, D7, D8, false, 95, 11);
+  testSerial.begin(115200, EspSoftwareSerial::SWSERIAL_8N1, D7, D8, false, 95, 11);
   
   // Initialize Firebase
   Firebase.begin(firebaseHost, firebaseAuth);
@@ -43,51 +43,51 @@ void loop() {
 
   if(testSerial.available() > 0){
     int cnt = 0;
+    Serial.println("Have some!");
     while(cnt != 12){
-      while(testSerial.available() > 0){
+      if(testSerial.available() > 0){
         buffer[cnt++] = testSerial.read();
-        // Serial.print("Received: ");
-        // Serial.println(receivedData);
-        // Serial.println(buffer[cnt-1]);
+        Serial.print("Received: ");
+        Serial.println(buffer[cnt-1]);
       }
     }
     buffer[cnt] = NULL;
     Serial.println(buffer);
 
-    int TDS_Value = 0;
-    for(int i=1;i<=3;i++){
-      TDS_Value*=10;
-      TDS_Value+=buffer[i]-'0';
-    }
-    int Thermo_Value = 0;
-    for(int i=5;i<=7;i++){
-      Thermo_Value*=10;
-      Thermo_Value+=buffer[i]-'0';
-    }
-    int DissolvedO2_Value = 0;
-    for(int i=9;i<=11;i++){
-      DissolvedO2_Value*=10;
-      DissolvedO2_Value+=buffer[i]-'0';
-    }
-    // Send the data to Firestore
-    if (Firebase.setInt(firebaseData, "/TDS_Value", TDS_Value)) {
-      Serial.println("TDS_Value sent to Firestore successfully");
-    } else {
-      Serial.println("Failed to send data to Firestore");
-      Serial.println(firebaseData.errorReason());
-    }
-    if (Firebase.setInt(firebaseData, "/Thermo_Value", Thermo_Value)) {
-      Serial.println("Thermo_Value sent to Firestore successfully");
-    } else {
-      Serial.println("Failed to send data to Firestore");
-      Serial.println(firebaseData.errorReason());
-    }
-    if (Firebase.setInt(firebaseData, "/DissolvedO2_Value", DissolvedO2_Value)) {
-      Serial.println("DissolvedO2_Value sent to Firestore successfully");
-    } else {
-      Serial.println("Failed to send data to Firestore");
-      Serial.println(firebaseData.errorReason());
-    }
+    // int TDS_Value = 0;
+    // for(int i=1;i<=3;i++){
+    //   TDS_Value*=10;
+    //   TDS_Value+=buffer[i]-'0';
+    // }
+    // int Thermo_Value = 0;
+    // for(int i=5;i<=7;i++){
+    //   Thermo_Value*=10;
+    //   Thermo_Value+=buffer[i]-'0';
+    // }
+    // int DissolvedO2_Value = 0;
+    // for(int i=9;i<=11;i++){
+    //   DissolvedO2_Value*=10;
+    //   DissolvedO2_Value+=buffer[i]-'0';
+    // }
+    // // Send the data to Firestore
+    // if (Firebase.setInt(firebaseData, "/TDS_Value", TDS_Value)) {
+    //   Serial.println("TDS_Value sent to Firestore successfully");
+    // } else {
+    //   Serial.println("Failed to send data to Firestore");
+    //   Serial.println(firebaseData.errorReason());
+    // }
+    // if (Firebase.setInt(firebaseData, "/Thermo_Value", Thermo_Value)) {
+    //   Serial.println("Thermo_Value sent to Firestore successfully");
+    // } else {
+    //   Serial.println("Failed to send data to Firestore");
+    //   Serial.println(firebaseData.errorReason());
+    // }
+    // if (Firebase.setInt(firebaseData, "/DissolvedO2_Value", DissolvedO2_Value)) {
+    //   Serial.println("DissolvedO2_Value sent to Firestore successfully");
+    // } else {
+    //   Serial.println("Failed to send data to Firestore");
+    //   Serial.println(firebaseData.errorReason());
+    // }
     // Delay before sending the next data
     // delay(5000);
   }
